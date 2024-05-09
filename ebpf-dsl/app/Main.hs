1b37fc455a10793703295c98e84e999efae44b36
Bruno Braga <brunoarbraga@gmail.com>
2024-05-09 18:23:56 -0300

    Add files via upload

--- ebpf-dsl/app/Main.hs
+++ ebpf-dsl/app/Main.hs
@@ -3,9 +3,11 @@ module Main where
 import DSL
 import FileGen
 import Hlist
+import Hlist (hLookup)
 
 main :: IO ()
 main = do
-  writeCFile "fact.c" factorial
-  writeCFile "sum.c" sumInput
+  --writeCFile "fact.c" factorial
+  --writeCFile "sum.c" sumInput
+  hLookup 
   print example3
--- ebpf-dsl/src/Hlist.hs
+++ ebpf-dsl/src/Hlist.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
@@ -12,35 +11,64 @@
 
 
 
-
 module Hlist where
 
+
+--imports
 import Data.Kind (Type)
 import Control.Monad()
 import Data.Proxy
+import Data.Typeable
 -- import GHC.Base (Symbol)
 import GHC.TypeLits 
---import Hlist (Pair(MkPair))
 
 
 
+{-
 
-data HList (ts :: [(Symbol, Type)]) where 
+Type definition por heterogeneous list
+HNil is empty list
+Cons operator :# takes a pair of a symbol("promoted" string) and a type(Int, String...) and adds it to an existing list
+Basically, every element of the list represents an atribute of a struct in C. 
+
+
+Example struct we want to represent:
+    typedef struct{
+        String Nome;
+        Int Idade;
+    }Aluno;
+
+
+It's representation as an heterogeneous list in Haskell:
+    [("nome", String), ("Idade", Int)]
+
+-}
+data HList (xs :: [(Symbol, Type)]) where 
     HNil :: HList '[]
-    (:#) :: Pair s t -> HList ts -> HList ('(s, t) ': ts) 
+    (:#) :: Pair s t -> HList xs -> HList ('(s, t) ': xs) 
 infixr 5 :#
 
 
 
+
+
+
+--Instance of Show for the list
+
 instance Show (HList '[]) where
     show HNil = ""
 
-instance (Show t, Show (HList ts)) => Show (HList ('(s, t) ': ts)) where
-    show :: (Show t, Show (HList ts)) => HList ('(s, t) : ts) -> String
+--Prints the pair of the head and does it recursively for the tail
+instance (Show t, Show (HList xs)) => Show (HList ('(s, t) ': xs)) where
+    show :: (Show t, Show (HList xs)) => HList ('(s, t) : xs) -> String
     show (x :# xs) = showTuple x ++ "\n" ++ show xs where
         showTuple (MkPair name value) = show (symbolVal name) ++ " = " ++ show value
 
 
+
+
+
+--Pait type that stores the name of the struct field and it's type 
 data Pair (s :: Symbol)(t :: Type) where 
   MkPair :: KnownSymbol s => Proxy s -> t -> Pair s t 
 
@@ -48,34 +76,55 @@ data In (s :: Symbol)(t :: Type)(xs :: [(Symbol, Type)]) where
   Here :: In s t ('(s , t) ': xs)
   There :: In s t xs -> In s t ( '(s1 , t1) ': xs)
 
--- val.x
---  
--- hLookup :: In s t xs -> HList xs -> t 
--- hLookup Here ((MkPair _ v) :# xs) = v 
--- hLookup (There p) (_ :# xs) = hLookup p xs
-
-example3 :: HList '[ '("nome", String)]
-example3 = (MkPair (Proxy :: Proxy "nome") "Bruno") :# HNil -- Proxy :: (Proxy "nome", "Bruno") :# HNil
 
 
 
 
 
+type family Getter (s :: Symbol) (xs :: [(Symbol, Type)]) :: Type where
+  Getter s ('(s, t) ': xs) = t
+  Getter s ('(s1, t1) ': xs) = Getter s xs
+
+type family Setter (s :: Symbol) (t :: Type) (xs :: [(Symbol, Type)]) :: [(Symbol, Type)] where
+  Setter s t '[] = '[]
+  Setter s t ('(s, t) ': xs) = xs
+  Setter s t ('(s1, t1) ': xs) = Setter s t xs
 
 
--- type family All (c :: Type -> Constraint)
---     (ts :: [Type]) :: Constraint where
---         All c '[] = () 
---         All c (t ': ts) = (c t, All c ts)
 
--- instance All Eq ts => Eq (HList ts) where
---     HNil == HNil = True
---     (x :# xs) == (y :# ys) = x == y && xs == ys
 
--- instance (All Eq ts, All Ord ts) => Ord (HList ts) where
---     compare HNil HNil = EQ
---     compare (x :# xs) (y :# ys) = 
---         case compare x y of
---             LT -> LT
---             GT -> GT
---             EQ -> compare xs ys
+hLookup :: In s t xs -> HList xs -> t 
+hLookup Here ((MkPair _ v) :# xs) = v 
+hLookup (There p) (_ :# xs) = hLookup p xs
+
+
+
+
+hSetter :: KnownSymbol s => Proxy s -> t -> HList xs -> HList (Setter s t xs)
+hSetter _ _ HNil = HNil
+hSetter fieldName newValue ((MkPair name value) :# xs) = if symbolVal fieldName == symbolVal name
+                                                    then MkPair name newValue :# xs
+                                                    else MkPair name value :# hSetter fieldName newValue xs
+
+
+hGetter :: KnownSymbol s => Proxy s -> HList xs -> Getter s xs
+hGetter s xs = hLookup (findSymbol s xs) xs
+
+findSymbol :: KnownSymbol s => Proxy s -> HList xs -> In s t xs
+findSymbol s ((MkPair symbol value) :# xs) = if symbolVal s == symbolVal symbol 
+                                                then Here 
+                                                else There (findSymbol s xs) 
+
+
+
+
+
+
+
+
+
+
+
+
+example3 :: HList '[ '("Nome", String), '("Idade", Int)]
+example3 = MkPair (Proxy :: Proxy "Nome") "Bruno" :# MkPair (Proxy :: Proxy "Idade") 20 :# HNil -- Proxy :: (Proxy "nome", "Bruno") :# HNil
